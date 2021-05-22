7911bbad423620418f398dbced923ef9d6d11837
AhmedRushdy <43193251+AhmedRushdy@users.noreply.github.com>
2021-05-22 23:37:21 +0200

    order fragment

---
+++ app/src/main/java/com/ar/gptamwena/ui/fragments/FinishOrderFragment.kt
@@ -0,0 +1,21 @@
+package com.ar.gptamwena.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ar.gptamwena.R
+
+class FinishOrderFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_finish_order, container, false)
+    }
+
+
+}

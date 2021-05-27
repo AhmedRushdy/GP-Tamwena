a9cee78ea93252341e1c0db7785efad206c92473
Abdelrahman <dsaa2295@gmail.com>
2021-05-28 00:57:34 +0200

    UI updates2

---
+++ app/src/main/java/com/ar/gptamwena/ui/fragments/ProductsFragment.kt
@@ -0,0 +1,17 @@
+package com.ar.gptamwena.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ar.gptamwena.R
+
+class ProductsFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.product_fragment, container, false)
+    }
+}
--- app/src/main/java/com/ar/gptamwena/ui/shopprofile/ShopProfileActivity.kt
+++ app/src/main/java/com/ar/gptamwena/ui/shopprofile/ShopProfileActivity.kt
@@ -2,9 +2,15 @@ package com.ar.gptamwena.ui.shopprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.ar.gptamwena.R
-import com.ar.gptamwena.ui.shopprofile.SectionsPagerAdapter
+import com.ar.gptamwena.ui.fragments.AboutUsFragment
+import com.ar.gptamwena.ui.fragments.ContactUsFragment
+import com.ar.gptamwena.ui.fragments.ProductsFragment
+import com.ar.gptamwena.ui.fragments.RateUS
 import com.google.android.material.tabs.TabLayout
 
 class ShopProfileActivity : AppCompatActivity() {
@@ -12,12 +18,43 @@ class ShopProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
+        setupViewPager(viewPager)
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
     }
 
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(ProductsFragment(), "Products")
+        adapter.addFragment(RateUS(), "Rate Us")
+        adapter.addFragment(AboutUsFragment(), "About Us")
+        adapter.addFragment(ContactUsFragment(), "Contact Us")
+
+        viewPager.adapter = adapter
+    }
+
+    internal inner class ViewPagerAdapter(manager: FragmentManager) :
+        FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return mFragmentTitleList[position]
+        }
+    }
 }
